dc35c97cfbd7b1fe1b32f718865f697cdc856165
Yamagishi FMT <thiago_yamagishi@hotmail.com>
2024-04-02 16:14:09 -0300

    Commit Inicial

--- src/main/java/br/com/fullstack/moduloumsemanaonze/ModuloUmSemanaOnzeApplication.java
+++ src/main/java/br/com/fullstack/moduloumsemanaonze/ModuloUmSemanaOnzeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ModuloUmSemanaOnzeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ModuloUmSemanaOnzeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ModuloUmSemanaOnzeApplication.class, args);
+    }
 
 }
