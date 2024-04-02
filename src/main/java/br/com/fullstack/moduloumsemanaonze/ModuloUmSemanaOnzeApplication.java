fe11174bb1d6202bd64848b9a0901ecc20a11643
Yamagishi FMT <thiago_yamagishi@hotmail.com>
2024-04-02 15:48:36 -0300

    Initial commit

---
+++ src/main/java/br/com/fullstack/moduloumsemanaonze/ModuloUmSemanaOnzeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.moduloumsemanaonze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloUmSemanaOnzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloUmSemanaOnzeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fullstack/moduloumsemanaonze/ModuloUmSemanaOnzeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.moduloumsemanaonze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloUmSemanaOnzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
